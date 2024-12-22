7b446111f6ef293cadf708184de155a6dff156f8
Shikhar Arora <shikhar3@ualberta.ca>
2024-12-22 10:17:38 -0700

    Fix: for netlify deployment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/views')); // html
+app.use(express.static(__dirname)); // html
 app.use(express.static(__dirname + '/public')); // js, css, images
 
 const server = app.listen(5000);
